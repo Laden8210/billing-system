a0953559243e3f2289b40903bd2f8c6613e06715
Laden8210 <domingo.laden@gmail.com>
2024-10-07 21:10:40 +0800

    sgfs

--- resources/views/report/remittancereport.blade.php
+++ resources/views/report/remittancereport.blade.php
@@ -107,7 +107,7 @@
         <tbody>
             @foreach ($remittances as $remittance)
                 <tr>
-                    <td>{{ $remittance->em_fname }}</td>
+                    <td>{{ $remittance->employee->em_fname }} {{ $remittance->employee->em_lname }}</td>
                     <td>{{ $remittance->rm_date }}</td>
                     <td>{{ $remittance->rm_amount }}</td>
                     <td>
