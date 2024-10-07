68e43a442931bb8d38e3dd1689bf9ae13fce9030
Laden8210 <domingo.laden@gmail.com>
2024-10-07 16:25:08 +0800

    update

--- resources/views/subscriber/view.blade.php
+++ resources/views/subscriber/view.blade.php
@@ -1,139 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Subscribers Report</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 20px;
-            background-color: #f5f5f5;
-        }
+@section('title', 'Subscriber Information')
 
-        .report-container {
-            background-color: #fff;
-            padding: 20px;
-            max-width: 900px;
-            margin: 0 auto;
-            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-        }
+@section('content')
 
-        h1 {
-            font-size: 24px;
-            text-align: left;
-            margin-bottom: 20px;
-        }
 
-        .report-header {
-            text-align: left;
-            margin-bottom: 30px;
-        }
+    <div class="flex justify-between p-2 w-full ">
 
-        h2 {
-            margin-top: 0;
-        }
+        @livewire('subscriber.view-subscriber', ['id' => $id])
 
-        table {
-            width: 100%;
-            border-collapse: collapse;
-            margin-bottom: 20px;
-        }
-
-        table thead {
-            background-color: #f0f0f0;
-        }
-
-        table th,
-        table td {
-            border: 1px solid #ddd;
-            padding: 8px;
-            text-align: left;
-            font-size: 12px;
-        }
-
-        table th {
-            font-weight: bold;
-        }
-
-        .summary,
-        .prepared-by {
-            margin-top: 20px;
-        }
-
-        .summary p,
-        .prepared-by p {
-            margin: 5px 0;
-            font-size: 16px;
-        }
-
-        .summary p strong,
-        .prepared-by p strong {
-            font-weight: bold;
-        }
-    </style>
-</head>
-
-<body>
-    <div class="report-container">
-        <h1>JCLC Subscribers Report</h1>
-        <div class="report-header">
-            <p><strong>Date:</strong> {{ now()->format('Y-m-d') }}</p>
-
-            <p><strong>Report Generated On:</strong> {{now()}}</p>
-            <p><strong>Area:</strong> Tupi</p>
-            <p><strong>Status:</strong> Active</p>
-        </div>
-
-        <table>
-            <thead>
-                <tr>
-                    <th>BILLINGID</th>
-                    <th>Subscriber Name</th>
-                    <th>Payment</th>
-                    <th>Due Date</th>
-                    <th>STATUS</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($subscribers as $subscriber)
-                    <tr>
-
-                        <td>
-                            {{ $subscriber->subscriber_id }}
-                        </td>
-
-                        <td>
-                            {{ $subscriber->sr_fname . ' ' . $subscriber->sr_lname }}
-                        </td>
-
-                        <td>
-                            {{ $subscriber->sr_contactnum }}
-                        </td>
-
-                        <td>
-                            {{ $subscriber->sr_street . ' ' . $subscriber->sr_barangay . ' ' . $subscriber->sr_city }}
-                        </td>
-
-                        <td>
-                            {{ $subscriber->subscriptions->count() }}
-                        </td>
-
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-
-        <div class="summary">
-            <p><strong>Number of Subscriber:</strong> {{$subscribers->count()}}</p>
-        </div>
-
-        <div class="prepared-by">
-
-        </div>
     </div>
-</body>
 
-</html>
+
+@endsection
