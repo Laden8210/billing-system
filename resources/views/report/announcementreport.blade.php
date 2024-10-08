5da9584b2820fa605af09dfe052471ec5b4286da
Laden8210 <domingo.laden@gmail.com>
2024-10-08 09:33:19 +0800

    update

--- resources/views/report/announcementreport.blade.php
+++ resources/views/report/announcementreport.blade.php
@@ -72,7 +72,7 @@
     <div class="container">
         <header>
             <h1>JCLC Internet Servece</h1>
-            <p><strong>From:</strong>{{$start ."-".  $end}}</p>
+            <p><strong>From:</strong>{{$start ." - ".  $end}}</p>
 
             <hr>
         </header>
